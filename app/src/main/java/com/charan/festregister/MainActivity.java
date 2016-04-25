81c075026b80ac6a1eb284a0f3479232be097280
CharanNK <charkeet@gmail.com>
2016-04-25 14:10:51 +0530

    alpha commit

--- app/src/main/java/com/charan/festregister/MainActivity.java
+++ app/src/main/java/com/charan/festregister/MainActivity.java
@@ -1,9 +1,14 @@
 package com.charan.festregister;
 
+import android.annotation.TargetApi;
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,9 +16,42 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import okhttp3.FormBody;
+/*import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;*/
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+import java.util.*;
+
+import java.net.URLEncoder;
+
+import okhttp3.FormBody;
+import okhttp3.RequestBody;
 
 public class MainActivity extends AppCompatActivity {
-    Button button,reset;
+    Button button,reset,upload_button;
     String name,branch,sem,event,phone,college,message,code;
     EditText namefield,phonefield,collegefield;
     TextView tester;
@@ -25,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
 
         button = (Button)findViewById(R.id.button);
         reset = (Button)findViewById(R.id.reset_button);
+        upload_button = (Button) findViewById(R.id.upload_button);
 
         //Spinner for Branch
         Spinner branch_spinner = (Spinner) findViewById(R.id.branch_spinner);
@@ -62,7 +101,6 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 name = namefield.getText().toString();
                 phone = phonefield.getText().toString();
                 college = collegefield.getText().toString();
@@ -82,18 +120,20 @@ public class MainActivity extends AppCompatActivity {
         reset.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                namefield = (EditText) findViewById(R.id.namefield);
-                phonefield = (EditText) findViewById(R.id.phonefield);
-                collegefield = (EditText) findViewById(R.id.collegefield);
-
-                tester = (TextView) findViewById(R.id.tester);
-
                 name = namefield.getText().toString();
                 phone = phonefield.getText().toString();
                 college = collegefield.getText().toString();
 
-                code = new RandomString(6).nextString();
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+              //  new PostData().execute();
+            }
+        });
 
+        upload_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new PostData().execute();
             }
         });
     }
@@ -147,22 +187,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    public class OnEventSpinnerItemSelected implements AdapterView.OnItemSelectedListener{
+    public class OnEventSpinnerItemSelected implements AdapterView.OnItemSelectedListener {
 
         @Override
         public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-            switch (position){
-                case 1 : event = "COUNTER STRIKE";
+            switch (position) {
+                case 1:
+                    event = "COUNTER STRIKE";
                     break;
-                case 2 : event = "MINI MILITIA";
+                case 2:
+                    event = "MINI MILITIA";
                     break;
-                case 3 : event = "MAZE RUN";
+                case 3:
+                    event = "MAZE RUN";
                     break;
-                case 4 : event = "MIND IT";
+                case 4:
+                    event = "MIND IT";
                     break;
-                case 5 : event = "TECHQUIZIT";
+                case 5:
+                    event = "TECHQUIZIT";
                     break;
-                case 6 : event = "REVERSE CODING";
+                case 6:
+                    event = "REVERSE CODING";
                     break;
             }
         }
@@ -172,7 +218,50 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
+    public class PostData extends AsyncTask<Void,Void,Void>{
+        HttpURLConnection connection ;
+        @TargetApi(Build.VERSION_CODES.KITKAT)
+        @Override
+        protected Void doInBackground(Void... params) {
+            String posting_name = name;
+            String posting_branch = branch;
+            String posting_sem = sem;
+            String posting_event = event;
+            String posting_phone= phone;
+            String posting_college= college;
+            String posting_code= code;
+            String fullUrl = "https://docs.google.com/forms/d/1fy3QFvgLPVFHhC5FwFSHmyMtFgZNPbKyjoMW2UF9hIw/formResponse";
+            try {
+                URL url = new URL(fullUrl);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setDoOutput(true);
+                connection.setRequestMethod("POST");
+
+                List<NameValuePair> data = new ArrayList<NameValuePair>();
+                data.add(new BasicNameValuePair("entry_184103295", posting_name));
+//                data.add(new BasicNameValuePair("secondParam", paramValue2));
+//                data.add(new BasicNameValuePair("thirdParam", paramValue3));
+
+                OutputStream os = connection.getOutputStream();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
 
 
-    
+
+
+            String data = null;
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            Toast.makeText(getApplicationContext(),"Upload Complete !",Toast.LENGTH_LONG).show();
+        }
+    }
+
 }
